14a2a6c2f1cfa4fd31801a232791bae494fc6cde
Kapil Sachdeva <ksachdeva17@gmail.com>
2017-11-05 14:55:53 -0600

    simplify the generate-certificate script by using async/await

--- src/generate-certificate.ts
+++ src/generate-certificate.ts
@@ -28,15 +28,11 @@ setEngine('nodeEngine', nodeSpecificCrypto, new CryptoEngine({
 let certificateBuffer = new ArrayBuffer(0); // ArrayBuffer with loaded or created CERT
 let privateKeyBuffer = new ArrayBuffer(0);
 let trustedCertificates = []; // Array of root certificates from "CA Bundle"
-const intermadiateCertificates = []; // Array of intermediate certificates
-const crls = []; // Array of CRLs for all certificates (trusted + intermediate)
 
 const hashAlg = 'SHA-1';
 const signAlg = 'RSASSA-PKCS1-v1_5';
 
-function createCertificateInternal() {
-    // region Initial variables
-    let sequence: Promise<any> = Promise.resolve();
+async function createCertificateInternal(): Promise<void> {
 
     const certificate = new Certificate();
 
@@ -44,16 +40,12 @@ function createCertificateInternal() {
     let privateKey;
 
     trustedCertificates = [];
-    // endregion
 
-    // region Get a "crypto" extension
     const crypto = getCrypto();
     if (typeof crypto === 'undefined') {
         return Promise.reject('No WebCrypto extension found');
     }
-    // endregion
 
-    // region Put a static values
     certificate.version = 2;
     certificate.serialNumber = new asn1js.Integer({ value: 1 });
     certificate.issuer.typesAndValues.push(new AttributeTypeAndValue({
@@ -79,7 +71,6 @@ function createCertificateInternal() {
     // Extensions are not a part of certificate by default, it's an optional array
     certificate.extensions = [];
 
-    // region "BasicConstraints" extension
     const basicConstr = new BasicConstraints({
         cA: true,
         pathLenConstraint: 3
@@ -91,9 +82,7 @@ function createCertificateInternal() {
         extnValue: basicConstr.toSchema().toBER(false),
         parsedValue: basicConstr // Parsed value for well-known extensions
     }));
-    // endregion
 
-    // region "KeyUsage" extension
     const bitArray = new ArrayBuffer(1);
     const bitView = new Uint8Array(bitArray);
 
@@ -110,61 +99,40 @@ function createCertificateInternal() {
         extnValue: keyUsage.toBER(false),
         parsedValue: keyUsage // Parsed value for well-known extensions
     }));
-    // endregion
-    // endregion
 
-    // region Create a new key pair
-    sequence = sequence.then(() => {
-        // region Get default algorithm parameters for key generation
-        const algorithm = getAlgorithmParameters(signAlg, 'generatekey');
-        if ('hash' in algorithm.algorithm) {
-            algorithm.algorithm.hash.name = hashAlg;
-        }
-        // endregion
+    // create a new key pair
+    const algorithm = getAlgorithmParameters(signAlg, 'generatekey');
+    if ('hash' in algorithm.algorithm) {
+        algorithm.algorithm.hash.name = hashAlg;
+    }
 
-        return crypto.generateKey(algorithm.algorithm, true, algorithm.usages);
-    });
-    // endregion
-
-    // region Store new key in an interim variables
-    sequence = sequence.then((keyPair) => {
+    try {
+        const keyPair = await crypto.generateKey(algorithm.algorithm, true, algorithm.usages);
         publicKey = keyPair.publicKey;
         privateKey = keyPair.privateKey;
-    }, (error) => Promise.reject(`Error during key generation: ${error}`));
-    // endregion
+    } catch (error) {
+        throw new Error(`Error during key generation: ${error}`);
+    }
 
-    // region Exporting public key into "subjectPublicKeyInfo" value of certificate
-    sequence = sequence.then(() =>
-        certificate.subjectPublicKeyInfo.importKey(publicKey)
-    );
-    // endregion
+    // Exporting public key into "subjectPublicKeyInfo" value of certificate
+    await certificate.subjectPublicKeyInfo.importKey(publicKey);
 
-    // region Signing final certificate
-    sequence = sequence.then(() =>
-        certificate.sign(privateKey, hashAlg),
-        (error) => Promise.reject(`Error during exporting public key: ${error}`));
-    // endregion
+    try {
+        // signing final certificate
+        await certificate.sign(privateKey, hashAlg);
+    } catch (error) {
+        throw new Error(`Error during signing: ${error}`);
+    }
 
-    // region Encode and store certificate
-    sequence = sequence.then(() => {
-        trustedCertificates.push(certificate);
-        certificateBuffer = certificate.toSchema(true).toBER(false);
-    }, (error) => Promise.reject(`Error during signing: ${error}`));
-    // endregion
+    // Encode and store certificate
+    trustedCertificates.push(certificate);
+    certificateBuffer = certificate.toSchema(true).toBER(false);
 
-    // region Exporting private key
-    sequence = sequence.then(() =>
-        crypto.exportKey('pkcs8', privateKey)
-    );
-    // endregion
-
-    // region Store exported key on Web page
-    sequence = sequence.then((result) => {
-        privateKeyBuffer = result;
-    }, (error) => Promise.reject(`Error during exporting of private key: ${error}`));
-    // endregion
-
-    return sequence;
+    try {
+        privateKeyBuffer = await crypto.exportKey('pkcs8', privateKey);
+    } catch (error) {
+        throw new Error(`Error during exporting of private key: ${error}`);
+    }
 }
 
 function formatPEM(pemString) {
@@ -186,39 +154,32 @@ function formatPEM(pemString) {
     return resultString;
 }
 
-function createCertificate() {
-    return createCertificateInternal().then(() => {
-        const certificateString =
-            String.fromCharCode.apply(null, new Uint8Array(certificateBuffer));
+async function createCertificate() {
 
-        const base64Str = Buffer.from(certificateString).toString('base64');
+    await createCertificateInternal();
 
-        let resultString = '-----BEGIN CERTIFICATE-----\r\n';
-        resultString = `${resultString}${formatPEM(base64Str)}`;
-        resultString = `${resultString}\r\n-----END CERTIFICATE-----\r\n`;
+    const certificateString =
+        String.fromCharCode.apply(null, new Uint8Array(certificateBuffer));
 
-        console.log('Certificate created successfully!');
+    const base64Str = Buffer.from(certificateString).toString('base64');
 
-        const privateKeyString = String.fromCharCode.apply(null, new Uint8Array(privateKeyBuffer));
+    let resultString = '-----BEGIN CERTIFICATE-----\r\n';
+    resultString = `${resultString}${formatPEM(base64Str)}`;
+    resultString = `${resultString}\r\n-----END CERTIFICATE-----\r\n`;
 
-        const base64Str2 = Buffer.from(privateKeyString).toString('base64');
+    console.log('Certificate created successfully!');
 
-        resultString = `${resultString}\r\n-----BEGIN PRIVATE KEY-----\r\n`;
-        resultString = `${resultString}${formatPEM(base64Str2)}`;
-        resultString = `${resultString}\r\n-----END PRIVATE KEY-----\r\n`;
+    const privateKeyString = String.fromCharCode.apply(null, new Uint8Array(privateKeyBuffer));
 
-        // document.getElementById("new_signed_data").innerHTML = resultString;
+    const base64Str2 = Buffer.from(privateKeyString).toString('base64');
 
-        console.log(resultString);
+    resultString = `${resultString}\r\n-----BEGIN PRIVATE KEY-----\r\n`;
+    resultString = `${resultString}${formatPEM(base64Str2)}`;
+    resultString = `${resultString}\r\n-----END PRIVATE KEY-----\r\n`;
 
-        console.log('Private key exported successfully!');
-    }, (error) => {
-        if (error instanceof Object) {
-            console.log(error.message);
-        } else {
-            console.log(error);
-        }
-    });
+    console.log(resultString);
+
+    console.log('Private key exported successfully!');
 }
 
 createCertificate();
